afd9fbfca52993b8700f867f9dc0353f5c6c7c77
Arunpandiaraja <arun@designstring.com>
2023-11-04 12:14:03 +0530

    mobile layout removed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,19 +3,19 @@ import { Metadata } from "next";
 import * as React from "react";
 
 export const metadata: Metadata = {
-    title: "Home",
-    description: "Home page",
+  title: "Home",
+  description: "Home page",
 };
 
 export default function RootLayout(props: {
-    children: React.ReactNode;
-    widget: React.ReactNode;
+  children: React.ReactNode;
+  widget: React.ReactNode;
 }) {
-    return (
-        <html lang="en">
-            <body className="h-full bg-white mobLayout">
-                <main className="h-full">{props.children}</main>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body className="h-full bg-white">
+        <main className="h-full">{props.children}</main>
+      </body>
+    </html>
+  );
 }
