b89e07cdc4546e3813d8cda5168f84d7e9d3c25b
Arunpandiaraja <arun@designstring.com>
2023-11-04 12:14:43 +0530

    layout fix

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -4,7 +4,7 @@ import { Metadata } from "next";
 import * as React from "react";
 
 export const metadata: Metadata = {
-  title: "Clink-Gift Homepage",
+  title: "Homepage",
   description: "Home page",
 };
 
