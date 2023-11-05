ffc75ea38c258e0b102d0fa9ec2fbd9a378eb116
Punith B M <punith@designstring.com>
2023-11-06 00:38:27 +0530

    chain update to linea

--- utils/wagmi/index.tsx
+++ utils/wagmi/index.tsx
@@ -3,15 +3,12 @@ import "@rainbow-me/rainbowkit/styles.css";
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { ReactElement } from "react";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
-import { baseGoerli, polygonMumbai } from "wagmi/chains";
+import { baseGoerli, linea } from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
 
 import { productName, rainbowKitProjectId } from "../../constants";
 
-const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [baseGoerli],
-  [publicProvider()]
-);
+const { chains, publicClient, webSocketPublicClient } = configureChains([linea], [publicProvider()]);
 
 const { connectors } = getDefaultWallets({
   appName: "Proto",
